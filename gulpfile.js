24bf89ae4e01f8b9b3249b7395b4111bddf278a2
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-07-19 17:44:23 -0300

    Implement minifying and concatening files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 import { src, dest, series, parallel } from "gulp";
 import gulpSass from "gulp-sass";
 import * as sass from "sass";
+import concat from "gulp-concat";
+import uglify from "gulp-uglify";
 import browserSync from "browser-sync";
 
 function compileSass() {
@@ -11,4 +13,21 @@ function compileSass() {
     .pipe(browserSync.stream());
 }
 
-export default series(parallel(compileSass));
+function minifyAndConcatCss() {
+  return src(["src/*.css"])
+    .pipe(concat("styles.css"))
+    .pipe(dest("dist/css"))
+    .pipe(browserSync.stream());
+}
+
+function minifyAndConcatJs() {
+  return src(["src/*.js"])
+    .pipe(concat("script.js"))
+    .pipe(uglify())
+    .pipe(dest("dist/js"))
+    .pipe(browserSync.stream());
+}
+
+export default series(
+  parallel(compileSass, minifyAndConcatCss, minifyAndConcatJs)
+);
